14380ce79204d00b95f39dee221d379afd0df8a2
leahlovescoding <leahlovescoding@gmail.com>
2018-04-17 18:43:30 -0400

    added an orgo model

---
+++ Orgo fun/organicchem.py
@@ -0,0 +1,25 @@
+class functionalGroup:
+
+    def __init__(self, singlebond, doublebond, triplebond):
+        self.singlebond= sigmabond
+        self.doublebond = pisigmabond
+        self.triplebond= pipisigmabond
+
+    def classify_it():
+        if sigmabond:
+            (
+            print ("this could be an Ethane funtional group")
+            )
+        elif pisigmabond:
+            (
+            print ("this could be an Ethene funtional group")
+            )
+        elif pipisigmabond:
+            (
+            print ("this could be an Ethyne funtional group")
+            )
+
+
+Ethane = functionalGroup(sigmabond = 1)
+Ethene = functionalGroup(pisigmabond)
+Ethyne = functionalGroup(pipisigmabond)
--- code.py
+++ code.py
@@ -1,6 +1,22 @@
-import datetime 
-time = datetime.time
-date = datetime.date
+import datetime, timedelta
+import urllib
 
-print (time)
-print (date)
+class Times()
+    #A bunch of different times you are testing
+    def __init__(self, minute, second, hour):
+
+timenow = Time()
+timenow.datetime.now()
+    timelater= deltatime.addfiveminutes()
+
+class Date()
+    def  __init__(self, day, month, year):
+    self.datetime.now()
+datenow = Date()
+
+print (timenow.hour, ":", timenow.minute, ":", timenow.second)
+print(date)
+
+# import tkinter
+# from tkinter import Menu
+# from tkinter import ttk #themed TK
