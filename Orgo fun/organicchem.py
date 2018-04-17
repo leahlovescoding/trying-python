d97a911f2daa48915e13be1725ac81f8df6ddf5d
leahlovescoding <leahlovescoding@gmail.com>
2018-04-17 19:20:13 -0400

    orgo fun
    trying classes

--- Orgo fun/organicchem.py
+++ Orgo fun/organicchem.py
@@ -1,25 +1,29 @@
 class functionalGroup:
 
     def __init__(self, singlebond, doublebond, triplebond):
-        self.singlebond= sigmabond
-        self.doublebond = pisigmabond
-        self.triplebond= pipisigmabond
+        sigmabond= self.singlebond
+        pisigmabond= self.doublebond
+        pipisigmabond= self.triplebond
+
 
     def classify_it():
-        if sigmabond:
+        if sigmabond == 1:
             (
             print ("this could be an Ethane funtional group")
             )
-        elif pisigmabond:
+        elif pisigmabond == 1:
             (
             print ("this could be an Ethene funtional group")
             )
-        elif pipisigmabond:
+        elif pipisigmabond == 1:
             (
             print ("this could be an Ethyne funtional group")
             )
 
+if __name__ == '__main__':
+    group = functionalGroup
+    group.classify_it()
 
-Ethane = functionalGroup(sigmabond = 1)
-Ethene = functionalGroup(pisigmabond)
-Ethyne = functionalGroup(pipisigmabond)
+Ethane = functionalGroup(1, 0, 0)
+Ethene = functionalGroup(0, 1, 0)
+Ethyne = functionalGroup(0, 0, 1)
